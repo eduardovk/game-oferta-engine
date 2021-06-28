068b0b8e4cf58fb046a7234c4cc49b61dd5cecf0
eduardovk <eduardo_vk@hotmail.com>
2021-06-28 18:04:19 -0300

    Implementado request a API GameOferta para envio de e-mail

--- APIs/GameOfertaAPI.js
+++ APIs/GameOfertaAPI.js
@@ -3,7 +3,7 @@ const axios = require('axios'); //biblioteca para requisicoes http
 
 //Classe da API GameOferta
 class GameOfertaAPI {
-    
+
     constructor() {
         //recebe vars de ambiente de chaves e tokens de API
         this.keys = (config.ambient === 'localhost') ? require('./keys_local.js') : require('./keys.js');
@@ -11,12 +11,14 @@ class GameOfertaAPI {
 
     //envia solicitacao para API GameOferta notificar usuario de nova oferta
     async sendNotification(notificationInfo) {
-        var gamesIDs = "";
+        var gamesIDs = ""; //cria string de ids
         for (var newOffer of notificationInfo.new_offers) {
             gamesIDs += newOffer.id_game + ",";
         }
+        //remove ultima virgula da string
+        if (gamesIDs.slice(-1) == ',') gamesIDs = gamesIDs.slice(0, -1);
         console.log("Sending request for API Notification for user (" + notificationInfo.id_user + "), games (" + gamesIDs + ")");
-        //TODO
+        await axios.post('https://game-oferta-api.herokuapp.com/email', notificationInfo);
     }
 }
 
